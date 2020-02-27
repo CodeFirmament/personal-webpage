e20a1d35cff86e3de0ef4126d7dd35ec95dec0e6
CodeFirmament <workjorge@yahoo.com>
2020-02-27 12:29:44 -0500

    added a serial for changing the background+

--- scripts/change_codec.js
+++ scripts/change_codec.js
@@ -1,5 +1,5 @@
 function codec_change_color(color_base,color_comp){
-    console.log("ni");
+
     var block_elements = document.getElementsByClassName("text-color-base");
     var other_block_elements = document.getElementsByClassName("text-color-complement");
 
@@ -12,16 +12,44 @@ function codec_change_color(color_base,color_comp){
     }
 }
 
+function change_background(BG){
+    console.log(BG)
+    document.getElementsByTagName('body')[0].style.backgroundImage = "url('res/snow-bg.jpg')";
+    document.getElementsByTagName('body')[0].style.backgroundRepeat = "no-repeat)";
+    document.getElementsByTagName('body')[0].style.backgroundAttachment = "fixed";
+    document.getElementsByTagName('body')[0].style.backgroundSize = "cover";
+}
+
+function set_default(){
+
+    var block_elements = document.getElementsByClassName("text-color-base");
+    var other_block_elements = document.getElementsByClassName("text-color-complement");
+
+    for (let index = 0; index < block_elements.length; index++) {
+        block_elements[index].style = "";
+    }
+
+    for (let index = 0; index < other_block_elements.length; index++) {
+        other_block_elements[index].style = "";
+    }
+
+    
+    document.getElementsByTagName("body")[0].style = "";
+
+}
+
 function serial_management(){
     let input = document.getElementsByTagName("input")[1].value;
-    console.log("jeje: "+input)
+
     switch (input) {
         case "colorblue":
             codec_change_color("rgb(78, 110, 255)","rgb(135, 182, 253)");
             break;
-    
+        case "lonely":
+            change_background("snow-bg.jpg");
+            break;
         default:
-            console.log("nope");
+            set_default();
             break;
     }
 }
